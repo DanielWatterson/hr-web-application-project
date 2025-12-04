63d6feedf59a90ee70d0fac4fb4ffc87afd58371
DanielWatterson <46389716+DanielWatterson@users.noreply.github.com>
2025-12-04 14:24:51 +0200

    added in templates for the project

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,10 @@
+/* Home.vue */
+/* This is the default page that the user will see but will also be able to link back to from any other tabs. */
+
 <template>
   <div class="text-center mt-5">
     <h1>Welcome to ModernTech HR</h1>
-    <p>Your all-in-one HR management system</p>
+    <p>Your all-in-one HR management system! :D</p>
     <router-link to="/dashboard" class="btn btn-primary mt-3">Go to Dashboard</router-link>
   </div>
 </template>
