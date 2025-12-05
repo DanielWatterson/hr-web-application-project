a539bf4333da71dd0cc56cacb7a078e014ba0426
zukhanyerula987 <zukhanyerula987@gmail.com>
2025-12-05 15:04:32 +0200

    new files

--- src/pages/Performance.vue
+++ src/pages/Performance.vue
@@ -2,11 +2,33 @@
   <div>
     <h2>Performance Reviews Page</h2>
     <p>Performance review information will appear here.</p>
+
+        <ul class="list-group mt-3">
+      <li
+        v-for="employee in employees"
+        :key="employee.employeeId"
+        class="list-group-item"
+      >
+        <strong>{{ employee.name }}</strong><br>
+        Position: {{ employee.position }}<br>
+        Department: {{ employee.department }}
+      </li>  
+    </ul>
   </div>
+  
 </template>
 
 <script>
+
+import employeeData from '../data/employee_info.json'
+
 export default {
   name: "Performance",
+
+data() {
+  return {
+    employees: employeeData.employeeInformation
+    };
+  }
 };
 </script>
